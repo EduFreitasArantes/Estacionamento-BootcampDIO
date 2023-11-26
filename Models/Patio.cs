91b97834fb9a8300c23b052086fc813c254d837b
Edu Freitas <e.freitas.arante@gmail.com>
2023-11-26 09:10:21 -0300

    Commit inicial

---
+++ Models/Patio.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Globalization;
+
+namespace Estacionamento
+{
+    public class Patio
+    {
+        private decimal precoInicial = 0;
+        private decimal precoPorHora = 0;
+        private List<string> veiculos = new List<string>();
+
+        public Patio(decimal precoInicial, decimal precoPorHora)
+        {
+            this.precoInicial = precoInicial;
+            this.precoPorHora = precoPorHora;
+        }
+
+        public void AdicionarVeiculo()
+        {
+            Console.WriteLine("Digite a placa do veículo para estacionar:");
+            string placa = Console.ReadLine();
+
+            if (veiculos.Contains(placa))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Veículo com placa {placa} já está estacionado.");
+                Console.ResetColor();
+            }
+            else
+            {
+                veiculos.Add(placa);
+                Console.WriteLine($"Veículo com placa {placa} estacionado com sucesso!");
+                Console.WriteLine();
+            }
+        }
+
+        public void RemoverVeiculo()
+        {
+            Console.WriteLine("Digite a placa do veículo para remover:");
+            string placa = Console.ReadLine();
+
+            if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
+            {
+                Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
+
+                if (int.TryParse(Console.ReadLine(), out int horas))
+                {
+                    decimal valorTotal = precoInicial + (precoPorHora * horas);
+
+                    veiculos.Remove(placa);
+                    string valorFormatado = valorTotal.ToString("C", CultureInfo.GetCultureInfo("pt-BR"));
+
+
+                    Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: {valorFormatado}");
+                }
+                else
+                {
+                    Console.WriteLine("Valor de horas inválido.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
+            }
+        }
+
+        public void ListarVeiculos()
+        {
+            if (veiculos.Any())
+            {
+                Console.WriteLine("Os veículos estacionados são:");
+                foreach (string veiculo in veiculos)
+                {
+                    Console.WriteLine(veiculo);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Não há veículos estacionados.");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿namespace Estacionamento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            // Coloca o encoding para UTF8 para exibir acentuação
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            decimal precoInicial = 0;
+            decimal precoPorHora = 0;
+
+            Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\nDigite o preço inicial:");
+            string pI = Console.ReadLine().Replace(" ", "").Replace(".", ",");
+
+            if (decimal.TryParse(pI, out decimal pIDecimal))
+            {
+                precoInicial = pIDecimal;
+            }
+
+
+            Console.WriteLine("Agora digite o preço por hora:");
+            string pH = Console.ReadLine().Replace(" ", "").Replace(".", ",");
+
+            if (decimal.TryParse(pH, out decimal pHDecimal))
+            {
+                precoPorHora = pHDecimal;
+            }
+
+            Patio patio = new Patio(precoInicial, precoPorHora);
+
+            string opcao = string.Empty;
+            bool exibirMenu = true;
+
+            while (exibirMenu)
+            {
+                Console.Clear();
+                Console.WriteLine("Digite a sua opção:");
+                Console.WriteLine("1 - Cadastrar veículo");
+                Console.WriteLine("2 - Remover veículo");
+                Console.WriteLine("3 - Listar veículos");
+                Console.WriteLine("4 - Encerrar");
+
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        patio.AdicionarVeiculo();
+                        break;
+
+                    case "2":
+                        patio.RemoverVeiculo();
+                        break;
+
+                    case "3":
+                        patio.ListarVeiculos();
+                        break;
+
+                    case "4":
+                        exibirMenu = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Opção inválida");
+                        break;
+                }
+
+                Console.WriteLine("Pressione uma tecla para continuar");
+                Console.ReadLine();
+            }
+
+            Console.WriteLine("O programa se encerrou");
+        }
+    }
+}
